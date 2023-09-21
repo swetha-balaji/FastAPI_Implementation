9aa596db294b64dc5ca0ce19c595fd9db73043e4
swetha-balaji <swethab.uni@gmail.com>
2023-09-21 15:01:45 -0400

    feat: added files

---
+++ pythonProject/Assignment2/main.py
@@ -0,0 +1,21 @@
+import Data
+import cashier
+import sandwich_makerr
+from sandwich_makerr import SandwichMaker
+from cashier import Cashier
+
+
+# Make an instance of other classes here
+resources = Data.resources
+recipes = Data.recipes
+#sandwich_maker_instance =
+#cashier_instance =
+
+
+
+
+def main():
+
+
+#if __name__=="__main__":
+    main()
